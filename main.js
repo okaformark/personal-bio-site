c63eeee5cbb965fc21d357e562960c9f6f2b3754
Mark Okafor <okaformark@gmail.com>
2019-03-06 20:58:46 -0600

    created init function and called createProjectCards Func

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const printToDom = (divId, textToPrint) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML += textToPrint;
-};
 
 const projects= [
     {
@@ -13,11 +9,30 @@ const projects= [
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
   }
-]
+];
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
 
 const createProjectCards =() =>{
-    let domString ="";
+    let domString ='';
     for (i = 0; i < projects.length; i++){
-        domString += `<div class =fullPage `
+        domString += `<div class =fullPage> `;
+        domString += `<h1> ${projects[i].title}</h1>`;
+        domString += `<img src = "${projects[i].screenshot}">`;
+        domString += `<p> ${projects[i].description}</p>`;
+        domString += `<p> ${projects[i].technologiesUsed} </p>`;
+        domString += `<a href = ${projects[i].url}> <h3>GitHub Account</h3></a>`;
+        domString += `<a href = ${projects[i].githubUrl}> <h3>My GitHub URL</h3></a>`;
+        domString += `<p>${projects[i].available}`;
+        domString += `</div>`;
+        if(projects[i].available === true){
+            printToDom('projectsPage', domString);
+        }
     }
 };
+const init = () =>{
+    createProjectCards();
+};
+init();
